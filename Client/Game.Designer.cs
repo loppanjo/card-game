5cf383dcf7213c1ee3fbba79b430ebc682517677
Frosberg <Frosberg@FROSBERG>
2016-11-10 15:05:00 +0100

    Added Connect form. Added MenuStrip to Game form. Added basic functions for buttons etc.

--- Client/Game.Designer.cs
+++ Client/Game.Designer.cs
@@ -28,19 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemDisconnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameWindow1 = new Library.GameWindow();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuGame});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(408, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // menuGame
+            // 
+            this.menuGame.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemNewGame,
+            this.menuItemConnect,
+            this.menuItemDisconnect,
+            this.menuItemQuit});
+            this.menuGame.Name = "menuGame";
+            this.menuGame.Size = new System.Drawing.Size(50, 20);
+            this.menuGame.Text = "Game";
+            // 
+            // menuItemNewGame
+            // 
+            this.menuItemNewGame.Name = "menuItemNewGame";
+            this.menuItemNewGame.Size = new System.Drawing.Size(152, 22);
+            this.menuItemNewGame.Text = "New game";
+            this.menuItemNewGame.Click += new System.EventHandler(this.menuItemNewGame_Click);
+            // 
+            // menuItemConnect
+            // 
+            this.menuItemConnect.Name = "menuItemConnect";
+            this.menuItemConnect.Size = new System.Drawing.Size(156, 22);
+            this.menuItemConnect.Text = "Connect";
+            this.menuItemConnect.Click += new System.EventHandler(this.menuItemConnect_Click);
+            // 
+            // menuItemDisconnect
+            // 
+            this.menuItemDisconnect.Enabled = false;
+            this.menuItemDisconnect.Name = "menuItemDisconnect";
+            this.menuItemDisconnect.Size = new System.Drawing.Size(152, 22);
+            this.menuItemDisconnect.Text = "Disconnect";
+            this.menuItemDisconnect.Click += new System.EventHandler(this.menuItemDisconnect_Click);
+            // 
+            // menuItemQuit
+            // 
+            this.menuItemQuit.Name = "menuItemQuit";
+            this.menuItemQuit.Size = new System.Drawing.Size(156, 22);
+            this.menuItemQuit.Text = "Quit";
+            this.menuItemQuit.Click += new System.EventHandler(this.menuItemQuit_Click);
+            // 
+            // gameWindow1
+            // 
+            this.gameWindow1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameWindow1.Location = new System.Drawing.Point(0, 24);
+            this.gameWindow1.Name = "gameWindow1";
+            this.gameWindow1.Player = null;
+            this.gameWindow1.Size = new System.Drawing.Size(408, 283);
+            this.gameWindow1.TabIndex = 1;
+            this.gameWindow1.Text = "gameWindow1";
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(408, 307);
+            this.Controls.Add(this.gameWindow1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "Game";
-            this.Text = "Game";
+            this.ShowIcon = false;
+            this.Text = "Cardgame";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuGame;
+        private System.Windows.Forms.ToolStripMenuItem menuItemNewGame;
+        private System.Windows.Forms.ToolStripMenuItem menuItemConnect;
+        private System.Windows.Forms.ToolStripMenuItem menuItemDisconnect;
+        private System.Windows.Forms.ToolStripMenuItem menuItemQuit;
+        private Library.GameWindow gameWindow1;
     }
 }
--- Client/Game.cs
+++ Client/Game.cs
@@ -16,5 +16,44 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void menuItemNewGame_Click(object sender, EventArgs e)
+        {
+            /*
+             * Start a server and connect
+             */
+        }
+
+        private void menuItemConnect_Click(object sender, EventArgs e)
+        {
+            Connect connForm = new Connect();
+            while (true)
+            {
+                if (connForm.ShowDialog() == DialogResult.OK)
+                {
+                    //Use library method to connect
+                    //IF error
+                    //continue;
+                    //ELSE
+                    //break;
+                }
+                else
+                    break;
+            }
+            menuItemConnect.Enabled = false;
+            menuItemDisconnect.Enabled = true;
+        }
+
+        private void menuItemDisconnect_Click(object sender, EventArgs e)
+        {
+            //Use library method to disconnect
+            menuItemConnect.Enabled = true;
+            menuItemDisconnect.Enabled = false;
+        }
+
+        private void menuItemQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
