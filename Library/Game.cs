d3c809b735aa57d88187e04875cce534b8c680e4
Frosberg <Frosberg@FROSBERG>
2016-12-09 11:08:18 +0100

    Fixed Game

--- Library/Game.cs
+++ Library/Game.cs
@@ -12,8 +12,6 @@ namespace Library
 {
     public abstract class Game : Hub
     {
-        public const float RAD_TO_DEG = 180.0f / (float)Math.PI;
-
         public delegate void PlayerConnected(HubCallerContext context);
         
         public static event PlayerConnected PlayerConnectedEvent;
@@ -90,32 +88,5 @@ namespace Library
         }
 
         public abstract void Turn(Player player);
-
-        public void Draw(Graphics graphics)
-        {
-            float angle = (float)(Math.PI * 2) / Players.Count;
-            float hw = window.Width / 2;
-            float hh = window.Height / 2;
-            float min = Math.Min(window.Width, window.Height);
-            for (int i = 0; i < Players.Count; i++)
-            {
-                float x = hw + (float)Math.Cos(angle * i) * (min * 0.45f - Card.Height / 2);
-                float y = hh + (float)Math.Sin(angle * i) * (min * 0.45f - Card.Height / 2);
-
-                GraphicsState state = graphics.Save();
-                graphics.TranslateTransform(x, y);
-
-                float dx = x - hw;
-                float dy = y - hh;
-
-                graphics.TranslateTransform(Card.Width / 2, Card.Height / 2);
-                graphics.RotateTransform((float)Math.Atan2(dy, dx) * RAD_TO_DEG + 90);
-                graphics.TranslateTransform(-Card.Width / 2, -Card.Height / 2);
-
-                Players[i].Draw(graphics);
-
-                graphics.Restore(state);
-            }
-        }
     }
 }
